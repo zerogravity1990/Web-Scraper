eba8a79f658b1338f22187043e940bdf8bbbb24d
zerogravity1990 <tnt1984@libero.it>
2019-05-23 18:03:43 +0200

    general update

--- mangascraper/mangascraper/pipelines.py
+++ mangascraper/mangascraper/pipelines.py
@@ -27,17 +27,26 @@ class MyImagesPipeline(ImagesPipeline):
     # def get_media_requests(self, item, info):
     #     return [scrapy.Request(url, meta={'filename':item.get('image_name')}) for url in item.get(self.images_urls_field, [])]
 
+    # def file_path(self, request, response=None, info=None):
+    #     print "\n"*2
+    #     print scrapy.Request.meta.get(item['image_names'])
+    #     return scrapy.Request.meta.get(item['image_names'])
+    #     print "\n"*2
+
     def file_path(self, request, response=None, info=None):
-        return scrapy.Request.meta.get('filename','')
+        image_guid = scrapy.Request.item.get('image_names')
+        print "\n"*2 + image_guid + "\n"*2
+        return 'full/%s.jpg' % (image_guid)
 
     def get_media_requests(self, item, info):
-        img_url = item['image_urls'][0]
-        meta = {'filename': item['image_names']}
-        yield scrapy.Request(url=img_url, meta=meta)
+        for img_url in item['image_urls']:
+            # img_url = item['image_urls']
+            meta = {'filename': item['image_names']}
+            yield scrapy.Request(url=img_url, meta=meta)
 
     def item_completed(self, results, item, info):
         image_paths = [x['path'] for ok, x in results if ok]
         if not image_paths:
             raise DropItem("Item contains no images")
-        item['images'] = image_paths
+        item['images'] = image_paths # item['image_paths'] = image_paths
         return item
